34589c891edc847051b9fe9e69b4f2fd7747574a
KaiXoma <98159537+KaiXoma@users.noreply.github.com>
2023-03-13 21:17:42 +0700

    delete file expressjs, upload file angularjs

---
+++ shopping-NodejsAngular/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { ProductComponent } from './components/product/product.component';
+import { ServicesComponent } from './components/services/services.component';
+
+const routes: Routes = [
+  {path:'',component: HomeComponent},
+  {path:'product',component: ProductComponent},
+  {path:'lien-he',component: ContactComponent},
+  {path:'gio-hang',component: CartComponent},
+  {path:'gioi-thieu',component: AboutComponent},
+  {path:'dich-vu',component: ServicesComponent},
+  {path:'login',component: LoginComponent},
+  {path:'**',component: NotfoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
