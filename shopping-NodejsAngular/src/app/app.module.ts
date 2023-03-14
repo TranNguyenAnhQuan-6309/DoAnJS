205033139a59c0794e4f2c8b03b1564aae9e82c6
KaiXoma <98159537+KaiXoma@users.noreply.github.com>
2023-03-14 09:10:08 +0700

    upload thieu file src nen up lai

---
+++ shopping-NodejsAngular/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CartComponent } from './components/cart/cart.component';
+import { AboutComponent } from './components/about/about.component';
+
+
+@NgModule({
+  declarations: [
+    MainComponent,
+    HomeComponent,
+    ProductComponent,
+    NotfoundComponent,
+    ContactComponent,
+    CartComponent,
+    AboutComponent,
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [MainComponent]
+})
+export class AppModule { }
